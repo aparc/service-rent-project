8545ca7f8f43240f0effefa2e2ee90badc50c48c
aparc <a.parchukov@yandex.ru>
2018-02-16 15:58:46 +0300

    fixed controller

--- src/main/java/ru/aparc/controller/RegistrationUserController.java
+++ src/main/java/ru/aparc/controller/RegistrationUserController.java
@@ -2,10 +2,12 @@ package ru.aparc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import ru.aparc.domain.User;
+import ru.aparc.domain.UserInfo;
 import ru.aparc.facadeDao.UserFacadeDao;
 
 @Controller
@@ -18,8 +20,17 @@ public class RegistrationUserController {
         this.dao = dao;
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/userForm")
+    public String userForm(Model model) {
+        model.addAttribute("userForm", new User());
+        model.addAttribute("userInfoForm", new UserInfo());
+        return "user-form";
+    }
+
     @RequestMapping(method = RequestMethod.POST, value = "/addUser")
-    public String addUser(@ModelAttribute("user") User user) {
+    public String addUser(@ModelAttribute("userForm") User user, @ModelAttribute("userInfoForm") UserInfo userInfo) {
+        userInfo.setUser(user);
+        user.setInfo(userInfo);
         if(user.getUserId() == 0) {
             dao.createUser(user);
         } else {
